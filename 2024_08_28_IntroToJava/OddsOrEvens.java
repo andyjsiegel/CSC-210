f809c5f5e67a44d04ebd368025b938c9942421e1
andyjsiegel <andyjsiegel1@gmail.com>
2024-08-28 09:49:20 -0700

    Gradescope submission

--- 2024_08_28_IntroToJava/OddsOrEvens.java
+++ 2024_08_28_IntroToJava/OddsOrEvens.java
@@ -1,3 +1,5 @@
+package com.gradescope.oddeven;
+
 public class OddsOrEvens {
     public static boolean isEven(int number) { 
         return (number % 2 == 0);
@@ -6,10 +8,5 @@ public class OddsOrEvens {
     public static boolean isOdd(int number) { 
         return !(number % 2 == 0);
      }
-    public static void main(String[] args) {
-        int number = 4;
-        String result = (number % 2 == 0) ? "Even" : "Odd";
-        System.out.println(result);
-    }
 }
  
